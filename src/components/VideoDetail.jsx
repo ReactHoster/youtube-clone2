7ed52589e526d60a06e0bc4c2e9f4a192b1b1f18
ReactHoster <134359453+ReactHoster@users.noreply.github.com>
2023-09-19 09:09:35 +0900

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -32,7 +32,7 @@ const VideoDetail = () => {
 				<Box flex={1}>
 					<Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
 						<ReactPlayer
-							url={`https://www.youtube.com/watch?v=${id}`}
+							url={`https://www.youtube-nocookie.com/embed/${id}`}
 							className="react-player" controls
 						/>
 						<Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
